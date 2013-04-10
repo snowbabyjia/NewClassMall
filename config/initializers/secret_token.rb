625ddcb1aa445996b4e5959ed3a45233820ff477
Jiajia Zhao <snowbabyjia@gmail.com>
2013-04-10 19:59:11 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClassMallBdc::Application.config.secret_token = '815acc7322372df89e696dee9a08c87d4d7b379664a8a57d306558dcd6978f34becdf55ed1c87719d94684de224d7f5b5c4cc161068072fb536fe66ac34ab06d'
