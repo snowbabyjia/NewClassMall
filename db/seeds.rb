a4fd17ab09eb2ba8c98f00e0f77260fd7c370de0
Jiajia Zhao <snowbabyjia@gmail.com>
2013-04-10 22:29:49 -0400

    working copy. if want to start fresh with empty table: rake db:schema:dump, rake db:schema:load. Have roles: Admin, user.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
 puts 'ROLES'
 
 YAML.load(ENV['ROLES']).each do |role|
